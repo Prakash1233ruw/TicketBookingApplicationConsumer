cc50212fa4854166b740f853b73b65830915fd12
prakash <pr0018908@gmail.com>
2024-02-20 16:24:13 +0530

    main added

---
+++ src/main/java/com/pp/SpringRestApiTicketConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiTicketConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiTicketConsumerApplication.class, args);
+	}
+
+}
